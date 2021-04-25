11ed3e32d2843a5281122cf5110e1371af16e31d
Ashley Ramsey <aramsey@cyverse.org>
2021-04-25 16:25:03 -0700

    Update linux button mapping

--- button_mapping.py
+++ button_mapping.py
@@ -28,19 +28,19 @@ darwin = {
 }
 
 linux = {
-    0: "button cross",
-    1: "button circle",
-    2: "button triangle",
-    3: "button square",
+    0: "button square",
+    1: "button cross",
+    2: "button circle",
+    3: "button triangle",
     4: "bumper left",
     5: "bumper right",
     6: "trigger left",
     7: "trigger right",
     8: "select",
     9: "start",
-    10: "meta",
-    11: "left stick",
-    12: "right stick",
+    10: "left stick",
+    11: "right stick",
+    12: "meta",
     13: "touchpad",
     "left": "dpad left",
     "right": "dpad right",
@@ -49,22 +49,22 @@ linux = {
     "axis": {
         0: ["stick left", "x"],
         1: ["stick left", "y"],
-        3: ["stick right", "x"],
-        4: ["stick right", "y"]
+        2: ["stick right", "x"],
+        5: ["stick right", "y"]
     }
 }
 
 windows = {
     0: "button cross",
     1: "button circle",
-    2: "button square", #
-    3: "button triangle", #
-    4: "select", #
-    5: "meta", #
+    2: "button square",
+    3: "button triangle",
+    4: "select",
+    5: "meta",
     6: "start",
     7: "stick left",
     8: "stick right",
-    9: "bumper left", #
+    9: "bumper left",
     10: "bumper right",
     11: "dpad up",
     12: "dpad down",
